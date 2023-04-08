a587dec81a8ef0981996da1050a915355810c6b3
Munsif <munsifsolkar3@gmail.com>
2023-04-08 17:03:55 +0530

    added new features

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const exp = require("express");
+const mongo = require("mongodb");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = exp()
@@ -13,6 +14,54 @@ app.set('views',path.join(__dirname,'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
+//DATABASE INTREGATION
+let db;
+let collection;
+async function connectDatabase(uri,dbname){
+	const client = new mongo.MongoClient(uri,{useNewUrlParser:true,useUnifiedTopology:true});
+	try{
+		await client.connect();
+		db = client.db(dbname);
+		console.log("Connection to database successfull");
+	}
+	catch(err){
+		console.log(err);
+	}
+}
+const uri = "mongodb+srv://Munsif:wasteinocean@cropchop.x60cw.mongodb.net/?retryWrites=true&w=majority";
+const dbname = "texttrack";
+
+//dbclient
+class dbClient{
+        constructor(url_query){
+                this.url_query = url_query.params.x;
+                this.fetched_data;
+        }
+        async fetchAll(formatDate=false){
+                this.fetched_data = await collection.find({'custom_url':this.url_query}).toArray();
+                if(formatDate && this.fetched_data.length > 0){
+                        const e = this.fetched_data[0]['date'];
+                        this.fetched_data[0]['date'] = `${e.getDate()}/${e.getMonth()+1}/${e.getFullYear()} ${e.getHours()}:${e.getMinutes()}`;
+                }
+                return this.fetched_data[0];
+        }
+        exists(){
+                if(this.fetched_data.length > 0){
+                        return true;
+                }else{
+                        return false;
+                }
+        }
+        edit_mode(error=0,setEditMode={}){                                                                                                                                              setEditMode['edit_code'] = '';
+                setEditMode['mode'] = 'edit';
+                setEditMode['error'] = error;
+                return setEditMode;
+        }
+        async update(updatedData){
+                await collection.updateOne({'custom_url':this.url_query},{$set:updatedData});
+        }
+}
+
 
 //ROUTES
 app.get('/',function(req,res){
@@ -21,25 +70,29 @@ app.get('/',function(req,res){
 		'text_title':"",
 		'text_body':"",
 		'edit_code':"",
-		'custom_url':""
+		'custom_url':"",
+		'mode':'create'
 	}
 	res.render('home',query);
 })
 
 
-function verifyEntries(th,tb,ec,cu){
+function verifyEntries(e){
 	//text header
-	th = th.trim();
-	th = (th && th.length <=  20);
+	th = e.text_header.trim();
+	th = (th && th.length <=  40);
 	//text body
-	tb = tb.trim();
-	tb = (tb && tb.length <= 900);
+	tb = e.text_body.trim();
+	tb = (tb && tb.length <= 2000);
 	//edit code
-	ec = ec.replace(" ", "").trim();
-	ec = (ec && ec.length <= 9);
+	ec = e.edit_code.replace(" ", "").trim();
+	ec = (ec && ec.length <= 9 && ec.length >= 5);
 	//custom url
-	cu = cu.replace(/[^a-zA-Z0/9]/g,'').trim();
-	cu = (cu && cu.length <= 9);
+	cu = e.custom_url;
+	var raw_cu = cu
+	cu = cu.replace(/[^a-zA-Z0-9]/g,'').trim();
+	if(raw_cu != cu){cu=''};
+	cu = (cu && cu.length <= 20 && cu.length >= 5);
 	if(!th){
 		throw "Please check your Title Field it must contain minimum of 1 character and max 20 characters are allowed.";
 	}
@@ -56,32 +109,115 @@ function verifyEntries(th,tb,ec,cu){
 		return true;
 	}
 }
-app.post('/',function(req,res){
+app.post('/',async function(req,res){
 	const fdata = req.body;
 	var text_header = fdata.text_header;
 	var text_body = fdata.text_body;
 	var edit_code = fdata.edit_code.replace(" ","");
 	var custom_url = fdata.custom_url.replace(" ","");
 	try{
-		verifyEntries(text_header,text_body,edit_code,custom_url);
-		res.send("success");
+		verifyEntries(fdata);
+		let date = new Date();
+		//check url existence
+		const url_available = await collection.find({'custom_url':custom_url}).toArray();
+		if(url_available.length > 0){
+		    throw "URL not available!";
+		}
+		await collection.insertOne({
+					'text_header':text_header,
+					'text_body':text_body,
+					'edit_code':edit_code,
+					'custom_url':custom_url,
+					'date':date
+				});
+		console.log("successfully added data");
+		res.redirect("/"+custom_url);
 	}
 	catch(err){
 		console.log(err);
-		var error_message = "Error: "+err;
-		var onError = {
-			'error':error_message,
-			'text_title':text_header,
-			'text_body':text_body,
-			'edit_code':edit_code,
-			'custom_url':custom_url
-		};
-		res.render('home',onError);
+		var error_message = err;
+		//var onError = {
+		//	'error':error_message,
+		//	'text_header':text_header,
+		//	'text_body':text_body,
+		//	'edit_code':edit_code,
+		//	'custom_url':custom_url
+		//};
+		fdata['error'] = error_message;
+		fdata['mode'] = 'create';
+		console.log(fdata);
+		res.render('home',fdata);
 	}
 })
 
-var server = app.listen(5500, function() {
-   var host = server.address().address;
-   var port = server.address().port;
-   console.log("Listening on port %s", port)
+
+//view page
+app.get('/:x',async (req,res)=>{
+	const dbc = new dbClient(req);
+	dbc.fetchAll(formatDate=true).then(function(fetchedData){
+	  if(dbc.exists()){
+	      data = fetchedData;
+	      res.render('view_text',data);
+	  }else{
+	      res.send("Nothing found here!!");
+	  }
+	});
 })
+//edit
+app.get("/edit/:x",async (req,res)=>{
+	console.log(req.hostname);
+	const dbc = new dbClient(req);
+	dbc.fetchAll().then(function(fetched_data){
+		if(dbc.exists()){
+                    data = dbc.edit_mode(error=0,setEditMode=fetched_data);
+		    console.log(data);
+                    res.render('home',data);
+        	}else{
+		    res.send("nothing found here!!!!");
+		}
+	});
+})
+//SAVING EDITED DATA
+function verify_code(code,codex){
+	if(code != codex){
+		throw "incorrect edit code!";
+	}
+}
+
+app.post('/edit/:x',(req,res)=>{
+	const new_data = req.body;
+	console.log(new_data);
+	const dbc = new dbClient(req);
+	dbc.fetchAll().then(function(fetched_data){
+		if(dbc.exists()){
+			const date = fetched_data.date;
+			try{
+				verify_code(new_data.edit_code,fetched_data.edit_code);
+				verifyEntries(new_data);
+				new_data['date'] = date;
+				dbc.update(new_data);
+				res.redirect('/'+new_data.custom_url);
+			}
+			catch(err){
+				new_data['custom_url'] = fetched_data.custom_url;
+				data = dbc.edit_mode(error=err,new_data)
+				res.render('home',data);
+			}
+		}else{
+			res.json({"error":"Invalid request path."});
+		}
+	})
+})
+
+
+//START SERVER ON SUCCESSFULL DATABASE CONNECTION;
+connectDatabase(uri,dbname).then(function(){
+        collection = db.collection("text_data");
+	console.log("Starting server");
+	var server = app.listen(5500, function(err) {
+  		if(err){console.log(err)};
+		var port = server.address().port;
+   		console.log("Listening on port %s", port);
+
+	})
+});
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,17 +1,27 @@
 var app= angular.module("texttrack", []);
         app.controller("page-control", ['$scope', function($app){
+		$app.text_header = attrVal('.page-title');
+		$app.text_body = attrVal('.textbox');
+		$app.page_key = attrVal('.page-key');
+		$app.custom_url = attrVal('.custom-url');
+		//random string generator
             $app.genRandom = function(){
                 const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const length=6;
     let result = '';
     const charactersLength = characters.length;
     for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));     
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
             };
             return result;
             }
+	     	//ends
         }])
         function copyToClipboard(e){
             e.select();
             document.execCommand('copy');
         }
+	function attrVal(query){
+		var val = document.querySelector(query).value;
+		return val.trim();
+	}
