c31729dae53207aded9792d7a1406814f090bffc
Munsif <munsifsolkar3@gmail.com>
2023-05-19 18:56:23 +0530

    SEt new criteria for inputs and created separate file for criteria

--- public/javascript/tagsConstrustor.js
+++ public/javascript/tagsConstrustor.js
@@ -7,7 +7,7 @@ app.controller('hashtags',function($scope){
 
 function constructTag(tags){
 	console.log(tags);
-	const tagCriteria = /#[a-zA-Z0-9]{1,20}\b/g;
+	const tagCriteria = /#[a-zA-Z0-9]{1,50}\b/g;
 	tags = tags.trim();
 	tags_matches = tags.match(tagCriteria);
 	if(tags_matches && tags_matches.length > 0 || tags_matches === null){
@@ -20,13 +20,13 @@ function constructTag(tags){
 }
 
 function addTag(tags){
-	if(tags_matches === null){
+	if(tags === null){
 		scope.$parent.tags = [];
 		return;
 	}
 	if(tags.length<=30){
 	    scope.$parent.tags = tags;
         }else{
-	    throw "You met Max tags limit (6) please remove some tags";
+	    throw "You met Max tags limit (30) please remove some tags";
 	}
 }
